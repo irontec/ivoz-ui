21e108d60e400edcdba68acba982dd642f98a1ae
Mikel <mikel@irontec.com>
2022-04-20 13:39:22 +0200

    show request errors on List component

--- src/components/List/Content/ListContentValue.tsx
+++ src/components/List/Content/ListContentValue.tsx
@@ -12,49 +12,49 @@ interface ListContentValueProps {
 
 const ListContentValue = (props: ListContentValueProps): JSX.Element => {
 
-    const { column, columnName, row, entityService } = props;
+  const { column, columnName, row, entityService } = props;
 
-    const ListDecorator = entityService.getListDecorator();
-    const customComponent = (column as ScalarProperty).component;
+  const ListDecorator = entityService.getListDecorator();
+  const customComponent = (column as ScalarProperty).component;
 
-    const loadingValue =
-      ((column as FkProperty).$ref && row[columnName] && !row[`${columnName}Id`] && (column as FkProperty).type !== 'file')
-      || (column as ScalarProperty).type === 'array' && Array.isArray(row[columnName]);
+  const loadingValue =
+    ((column as FkProperty).$ref && row[columnName] && !row[`${columnName}Id`] && (column as FkProperty).type !== 'file')
+    || (column as ScalarProperty).type === 'array' && Array.isArray(row[columnName]);
 
-    const enumValues: any = (column as ScalarProperty).enum;
-    const value = row[columnName];
-    const isBoolean = typeof value === "boolean";
+  const enumValues: any = (column as ScalarProperty).enum;
+  const value = row[columnName];
+  const isBoolean = typeof value === "boolean";
 
-    let response = value;
-    if (loadingValue || (row[columnName] === undefined && !customComponent)) {
+  let response = value;
+  if (loadingValue || (row[columnName] === undefined && !customComponent)) {
 
-      response = (
-        <Fade
-          in={true}
-          style={{
-            transitionDelay: '1000ms',
-          }}
-          unmountOnExit
-        >
-          <LinearProgress color="inherit" />
-        </Fade>
-      );
-    } else if (isBoolean && !enumValues && value) {
-      response = <StyledCheckBoxIcon />;
-    } else if (isBoolean && !enumValues) {
-      response = <StyledCheckBoxOutlineBlankIcon />;
-    } else if (row[`${columnName}Link`]) {
-      response =
-        <StyledTableRowFkLink to={row[`${columnName}Link`]}>
-          {value}
-        </StyledTableRowFkLink>
-    } else {
-      response = <ListDecorator field={columnName} row={row} property={column} />
-    }
+    response = (
+      <Fade
+        in={true}
+        style={{
+          transitionDelay: '1000ms',
+        }}
+        unmountOnExit
+      >
+        <LinearProgress color="inherit" />
+      </Fade>
+    );
+  } else if (isBoolean && !enumValues && value) {
+    response = <StyledCheckBoxIcon />;
+  } else if (isBoolean && !enumValues) {
+    response = <StyledCheckBoxOutlineBlankIcon />;
+  } else if (row[`${columnName}Link`]) {
+    response =
+      <StyledTableRowFkLink to={row[`${columnName}Link`]}>
+        {value}
+      </StyledTableRowFkLink>
+  } else {
+    response = <ListDecorator field={columnName} row={row} property={column} />
+  }
 
-    const prefix = column?.prefix || '';
+  const prefix = column?.prefix || '';
 
-    return (<>{prefix}{response}</>);
+  return (<>{prefix}{response}</>);
 }
 
 export default ListContentValue;

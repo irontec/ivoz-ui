ea88aa38085337ad121440c247d557204920eed4
Mikel Madariaga <mikel@irontec.com>
2022-06-02 11:51:40 +0200

    parse routeMap into the menu

---
+++ src/components/layout/Menu/Menu.tsx
@@ -0,0 +1,56 @@
+import { Box, Divider, Drawer, Toolbar, useTheme, useMediaQuery } from '@mui/material';
+import { RouteMap } from '../../../router/routeMapParser';
+import MenuBlock from './MenuBlock';
+
+interface menuProps {
+  routeMap: RouteMap
+}
+
+export default function Menu(props: menuProps): JSX.Element | null {
+
+  const { routeMap } = props;
+
+  const theme = useTheme();
+  const bigScreen = useMediaQuery(theme.breakpoints.up('md'));
+
+  if (!bigScreen) {
+    return null;
+  }
+
+  const width = 200;
+
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
+          width,
+          boxSizing: 'border-box',
+          borderRight: 'none',
+        },
+      }}
+    >
+      <Toolbar />
+      <Box sx={{ overflow: 'auto' }}>
+        {routeMap.map((routeMapBlock, key: number) => {
+
+          const lastItem = key === routeMap.length -1;
+
+          const styles: Record<string, string> = {paddingLeft: '10px'};
+          if (key === 0) {
+            styles.marginTop = '23px';
+          }
+
+          return (
+            <div key={key} style={styles}>
+              <MenuBlock routeMapBlock={routeMapBlock} />
+              {!lastItem && <Divider sx={{width: '90%'}}/>}
+            </div>
+          );
+        })}
+      </Box>
+    </Drawer>
+  );
+}
---
+++ src/components/layout/Menu/MenuBlock.tsx
@@ -0,0 +1,49 @@
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { useHistory } from "react-router-dom";
+import { isActionItem, RouteMapBlock, RouteMapItem } from '../../../router/routeMapParser';
+
+interface menuBlockProps {
+  routeMapBlock: RouteMapBlock
+}
+
+export default function MenuBlock(props: menuBlockProps): JSX.Element {
+
+  const { routeMapBlock } = props;
+  const { label, children } = routeMapBlock;
+  const history = useHistory();
+
+  return (
+    <List sx={{marginTop: '10px'}}>
+      <ListItem key={'label'} disablePadding>
+        <strong style={{fontSize: '0.8em'}}>{label}</strong>
+      </ListItem>
+      {(children || []).map((item: RouteMapItem, key: number) => {
+
+        if (isActionItem(item)) {
+          return null;
+        }
+
+        const { route, entity } = item;
+
+        if (!entity) {
+          return null;
+        }
+
+        const styles = key === 0
+          ? {paddingTop: 0}
+          : {};
+
+        return (
+          <ListItem key={key} disablePadding sx={styles}>
+            <ListItemButton onClick={() => { history.push(route || ''); }}>
+              <ListItemIcon sx={{minWidth: 0, marginRight: '10px',}}>
+                <entity.icon />
+              </ListItemIcon>
+              <ListItemText primary={entity.title} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}

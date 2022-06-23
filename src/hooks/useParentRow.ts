16150f16c0695b94a57afcbe69d9d6ddb17cd131
Mikel Madariaga <mmadariaga@irontec.com>
2022-06-23 18:39:46 +0200

    useParentRow can now accept parentId prop

--- src/hooks/useParentRow.ts
+++ src/hooks/useParentRow.ts
@@ -9,31 +9,35 @@ import { match } from 'react-router-dom';
 type useParentRowProps = {
     parentEntity: EntityInterface,
     match: match,
+    parentId?: string | number | undefined,
     cancelToken?: CancelToken,
 }
 
-const useParentRow = <T = EntityValues>(props: useParentRowProps): T | undefined => {
+const useParentRow = <T extends {} = EntityValues>(props: useParentRowProps): T | null | undefined => {
 
     const { parentEntity, match } = props;
+    let { parentId } = props;
 
     let { cancelToken } = props;
     if (!cancelToken) {
         [,cancelToken] = useCancelToken();
     }
 
-    const params = {...match.params} as Record<string, string>;
-    for (const idx in params) {
+    if (!parentId) {
+        const params = {...match.params} as Record<string, string>;
+        for (const idx in params) {
 
-      if (idx.indexOf('parent_id') === 0) {
-        continue;
-      }
+            if (idx.indexOf('parent_id') === 0) {
+                continue;
+            }
 
-      delete params[idx];
+            delete params[idx];
+        }
+
+        parentId = Object.values(params).pop() || '';
     }
 
-    const parentId = Object.values(params).pop() || '';
-
-    const [row, setRow] = useState<T | undefined>(undefined);
+    const [row, setRow] = useState<T | null | undefined>(undefined);
     const apiGet = useStoreActions((actions) => {
         return actions.api.get;
     });
@@ -42,6 +46,7 @@ const useParentRow = <T = EntityValues>(props: useParentRowProps): T | undefined
         () => {
 
             if (!parentId) {
+                setRow(null);
                 return;
             }
 

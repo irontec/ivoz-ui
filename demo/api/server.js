8f2c335c100494831f9a2435d4729716b56ed4ac
Mikel Madariaga <mikel@irontec.com>
2023-03-06 11:41:30 +0100

    added missing api endpoints

--- demo/api/server.js
+++ demo/api/server.js
@@ -22,22 +22,86 @@ server.post('/token/refresh', (req, res) => {
 });
 
 server.get('/docs.json', (req, res) => {
-
     const apiSpec = fs.readFileSync(
-        path.join(__dirname, 'docs.json'),
+        path.join(__dirname, 'assets/docs.json'),
         { encoding: 'utf8', flag: 'r' }
     );
 
     res.send(apiSpec);
 });
 
+server.get('/clients/available', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            name: 'Remote PBX',
+            domain: 'remote.net',
+        },
+    ]);
+});
+
+server.get('/users/:id/login_qr', (req, res) => {
+    const apiSpec = fs.readFileSync(
+        path.join(__dirname, 'assets/login_qr_code.png'),
+        { encoding: 'binary', flag: 'r' }
+    );
+
+    res.contentType('image/png');
+    res.end(apiSpec, 'binary');
+});
+
+server.post('/provision/preview', (req, res) => {
+    const response = 
+    `<account>
+        <title>disabled</title>
+        <password>disabled</password>
+    </account>`;
+    res.send(response);
+});
+
+server.post('/:entity/:id/notify_credentials', (req, res) => {
+    res.send(`{}`);
+});
+
+server.post('/clients/:userId/sync', (req, res) => {
+    res.send(`{}`);
+});
+
+server.post('/users/:id/switch_licenses', (req, res) => {
+    res.send(`{}`);
+});
+
+server.get('/administrator/:id/avatar', (req, res) => {
+    const apiSpec = fs.readFileSync(
+        path.join(__dirname, 'assets/avatar.jpg'),
+        { encoding: 'binary', flag: 'r' }
+    );
+
+    res.set('Content-Disposition', 'attachment');
+    res.contentType('image/jpeg');
+    res.end(apiSpec, 'binary');
+});
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 
 // Use default router
+server.use((req, res, next) => {
+    const intercept =
+        req.path.indexOf('/administrators') === 0
+        && req.method !== "GET"
+        Object.keys(req.body).length === 0;
+
+    if (intercept) {
+        res.end(`{}`);
+        return;
+    }
+
+    next();
+})
 server.use(router);
+
 server.listen(3000, () => {
     console.log('JSON Server is running');
 })

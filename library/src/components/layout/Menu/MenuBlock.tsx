d9dbaef495fd5f2eb7e838b154c7162f984ae4a8
Mikel Madariaga <mikel@irontec.com>
2023-03-16 18:08:38 +0100

    menu rework

--- library/src/components/layout/Menu/MenuBlock.tsx
+++ library/src/components/layout/Menu/MenuBlock.tsx
@@ -1,5 +1,12 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useStoreActions, useStoreState } from '../../../store';
+import SettingsIcon from '@mui/icons-material/Settings';
+import {
+  Collapse,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import { useState } from 'react';
 import {
   EntityItem,
   isActionItem,
@@ -7,108 +14,70 @@ import {
   RouteMapBlock,
   RouteMapItem,
 } from '../../../router/routeMapParser';
-
-import { ListItem, ListItemButton, ListItemText } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {
-  StyledAccordion,
-  StyledAccordionDetails,
-  StyledAccordionSummary,
-  StyledList,
-  StyledListItemIcon,
-} from './MenuBlock.styles';
 import MenuListItem from './MenuListItem';
+import { StyledMenuListSubItem } from './MenuListItem.styles';
 
 interface menuBlockProps {
-  idx: number;
   routeMapBlock: RouteMapItem | RouteMapBlock;
 }
 
 export default function MenuBlock(props: menuBlockProps): JSX.Element {
-  const { idx, routeMapBlock } = props;
-  
+  const { routeMapBlock } = props;
 
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const open = useStoreState((store) => store.menu.open);
-  const expandMenu = useStoreActions((actions) => actions.menu.expand);
-  const colapseMenu = useStoreActions((actions) => actions.menu.colapse);
+  const [open, setOpen] = useState(false);
 
   if (isEntityItem(routeMapBlock as RouteMapItem)) {
     const entity = (routeMapBlock as EntityItem).entity;
     return (
-      <MenuListItem 
+      <MenuListItem
         path={entity.path}
-        icon={(<entity.icon />)}
+        icon={<entity.icon />}
         text={entity.title}
       />
     );
   }
 
   const { label, children } = routeMapBlock as RouteMapBlock;
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
   return (
-    <StyledList>
-      <StyledAccordion
-        expanded={open.includes(idx)}
-        onChange={(event: React.SyntheticEvent, expanded: boolean) => {
-          if (!expanded) {
-            colapseMenu(idx);
-            return;
-          }
-          expandMenu(idx);
-          for (const value of open) {
-            if (value !== idx) {
-              colapseMenu(value);
+    <>
+      <ListItemButton onClick={handleClick}>
+        <ListItemIcon>
+          <SettingsIcon />
+        </ListItemIcon>
+        <ListItemText primary={label} />
+      </ListItemButton>
+      <Collapse in={open} timeout='auto' unmountOnExit>
+        <List component='div' disablePadding>
+          {(children || []).map((item: RouteMapItem, key: number) => {
+            if (isActionItem(item)) {
+              return null;
             }
-          }
-        }}
-      >
-        <StyledAccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <ListItem key={'label'} disablePadding>
-            <strong style={{ fontSize: '0.8em' }}>{label}</strong>
-          </ListItem>
-        </StyledAccordionSummary>
-        {(children || []).map((item: RouteMapItem, key: number) => {
-          if (isActionItem(item)) {
-            return null;
-          }
 
-          const { route, entity } = item;
+            const { route, entity } = item;
 
-          if (!entity) {
-            return null;
-          }
+            if (!entity) {
+              return null;
+            }
 
-          if (!route) {
-            return null;
-          }
+            if (!route) {
+              return null;
+            }
 
-          return (
-            <StyledAccordionDetails key={key}>
-              <ListItem disablePadding>
-                <ListItemButton
-                  dense={true}
-                  selected={location.pathname.indexOf(route) === 0}
-                  onClick={() => {
-                    navigate(route, {
-                      state: {
-                        referrer: location.pathname,
-                      },
-                    });
-                  }}
-                >
-                  <StyledListItemIcon>
-                    <entity.icon />
-                  </StyledListItemIcon>
-                  <ListItemText primary={entity.title} />
-                </ListItemButton>
-              </ListItem>
-            </StyledAccordionDetails>
-          );
-        })}
-      </StyledAccordion>
-    </StyledList>
+            return (
+              <StyledMenuListSubItem
+                key={key}
+                path={route}
+                icon={<entity.icon />}
+                text={entity.title}
+              />
+            );
+          })}
+        </List>
+      </Collapse>
+    </>
   );
 }

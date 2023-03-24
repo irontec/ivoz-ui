d726432db7feb13946d99bd9aff203cce4def9f6
ozabala <ozabala@irontec.com>
2023-03-24 09:46:38 +0100

    textfield fix, dropdown, switch and multilanguage styles

--- library/src/services/form/Field/Autocomplete/Autocomplete.styles.tsx
+++ library/src/services/form/Field/Autocomplete/Autocomplete.styles.tsx
@@ -2,11 +2,7 @@ import { styled } from '@mui/material';
 import Autocomplete from './Autocomplete';
 
 const StyledAutocomplete = styled(Autocomplete)(({ theme }) => {
-  return {
-    '&.changed label': {
-      color: theme.palette.info.main,
-    },
-  };
+  return {};
 });
 
 export default StyledAutocomplete;

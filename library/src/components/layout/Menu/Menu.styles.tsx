06280480a7f8c1e9c3734074f97b324b48568604
ozabala <ozabala@irontec.com>
2023-04-05 15:37:45 +0200

    desktop sidemenu styling

--- library/src/components/layout/Menu/Menu.styles.tsx
+++ library/src/components/layout/Menu/Menu.styles.tsx
@@ -1,12 +1,11 @@
-import { Divider, styled } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
+import { Divider, styled } from '@mui/material';
 
 import List from '@mui/material/List';
 
 export const StyledDivider = styled(Divider)(() => {
   return {
-    margin: '10px 0 10px',
-    width: '90%',
+    borderColor: 'var(--color-border)',
   };
 });
 
@@ -14,9 +13,6 @@ export const StyledMenuList = styled(List)(() => {
   return {
     width: '100%',
     maxWidth: 360,
-    '&.colapsed': {
-      border: '1px solid red',
-    },
   };
 });
 

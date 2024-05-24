2b26a0add7b56aa0191c2b08681bc83fdd14eb43
Mikel Madariaga <mikel@irontec.com>
2024-05-24 10:40:12 +0200

    enhanced datime format in lists

--- library/src/entities/DefaultEntityBehavior/ListDecorator.tsx
+++ library/src/entities/DefaultEntityBehavior/ListDecorator.tsx
@@ -59,7 +59,17 @@ const ListDecorator: ListDecoratorType = (props) => {
   }
 
   if (isPropertyScalar(property) && property.format === 'date-time') {
-    return value ? new Date(value).toLocaleString() : '';
+    const localStringOptions: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    };
+    return value
+      ? new Date(value).toLocaleString(navigator.language, localStringOptions)
+      : '';
   }
 
   if (!value && property.null) {
